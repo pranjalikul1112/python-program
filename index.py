ea6f88e316f3ee0d77415d2e789a97e831c32764
pranjalikul1112 <pranjalikul1112@gmail.com>
2024-07-15 13:13:23 +0530

    new change

--- index.py
+++ index.py
@@ -1,10 +1,25 @@
-# This program adds two numbers
+#string
+#str1 = "this is a stirng.\n we are creating it in python."
+#print(str1)
+#string length
+#str1 = "pranjali"
+#len1 = len(str1)
+#print(len1)
 
-num1 = 1.5
-num2 = 6.3
-
-# Add two numbers
-sum = num1 + num2
-
-# Display the sum
-print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+#str2 = "kul"
+#len2 = len(str2)
+#print(len2) 
+#final_str = str1+ " "+ str2
+#print(final_str)
+#print(len(final_str))
+#indexing
+#str = "pranjalikul"
+#ch = str[0]
+#print(ch)
+#slicing
+#str = "pranjali kul"
+#print(str[1:4])
+#print(str[-1:-4])
+#string function
+str = "I am studying python from apna college"
+print (str.endswith{"ege"})
