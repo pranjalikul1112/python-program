dd9c0f76b95fd95e72fff0ef49be8b06aad8e171
pranjalikul1112 <pranjalikul1112@gmail.com>
2024-07-16 15:53:49 +0530

    new programs

---
+++ index1.py
@@ -0,0 +1,75 @@
+#marks1 = 89.1
+#marks2 = 32.2
+#marks3 = 33.3
+#marks4 = 76.4
+#marks5 = 43.5
+
+#marks = [89.1,32.2,33.3,76.4,43.5]
+#print(marks)
+#print(len(marks))
+#print(marks[0])
+#print(marks[1])
+
+#student = ["pranjali",98.4,11,"pune"]
+#print(student[0])
+#student[0] = "prachi"
+#print(student)
+ 
+#marks = [89,78,67,56,45]
+#print(marks[-3:4])
+
+#list = [2,1,3]
+#list.append(4)
+#print(list)
+
+#list = [2,1,3] 
+#print(list.append)
+#print(list.sort(reverse=True))
+#print(list)
+
+#list = [2,1,3]
+#list.insert(1,5)
+#print(list)
+
+#list = [2,3,4,1,4]
+#list.pop(2)
+#print(list)
+
+#TUPLE
+# tup = (2,1,3,1)
+#print(tup[0])
+#print(tup[1])
+
+#tup = (1,2,3,4)
+#print(tup.index(2)) 
+#tup = (1,2,3,4)
+#print(tup.count(2))
+
+#practice program
+#WAP to ask the user to enter names od their 3 favorite movies and store them in a list
+#movies = []
+#mov1 = input("enter 1st movie: ")
+#mov2 = input("enter 2nd movie: ")
+#mov3 = input("enter 3rd movie: ")
+
+#movies.append(mov1)
+#movies.append(mov2)
+#movies.append(mov3)
+#print(movies)
+#WAP to check if a list contains a palindrome of elements .(hint: use copy()method)
+# [1,2,3,2,1]     [1,"abc","abc"1]
+
+#list1 = [1,2,1]
+#list2 = [1,2,3]
+#copy_list1 = list1.copy()
+#copy_list1.reverse()
+#if(copy_list1 == list1):
+#    print("palindrome")
+#else:
+#    print("NOT palindrome")
+#WAP to count the number of students with the "A"
+# grade in the following tuple
+# ["C","D","A","A","B","B","A"] 
+grade = ["C","D","A","A","B","B","A"] 
+grade.sort()
+print(grade)
