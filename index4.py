0708d0667d6a265684e7adbc8474b32bca9bfbd3
pranjalikul1112 <pranjalikul1112@gmail.com>
2024-07-25 22:40:23 +0530

    add new program

--- index4.py
+++ index4.py
@@ -1,1 +1,39 @@
-print("hello pranjali")
+#function
+#def calc_sum(a, b):
+ #   sum = a + b
+  #  print(sum)
+   # return sum
+#calc_sum(5,5)
+
+#calc_sum(2,2)
+
+#calc_sum(4,4)
+#function definition
+#def calc_sum(a, b):#parameters
+ #   return a + b
+#sum = calc_sum(123,345)#function call; argument
+#print(sum)
+
+#def print_hello():
+ #   print("hello")
+#print_hello()
+
+#average of 3 numbers
+#def calc_avg(a, b, c):
+ #   sum = a + b + c
+  #  avg = sum / 3
+   # print(avg)
+    #return avg
+#calc_avg(98,97,96)
+#def cal_prod(a=1, b=1):
+ #   print(a * b)
+  #  return a * b
+#cal_prod()
+#practice question
+#write a  program the length of a list .(list is the parameter)
+cities = ["delhi","gurgaon","noida","pune","mumbai","chennai"]
+heroes = ["thor","ironman","captain america","shaktiman"]
+print(cities)
+#write a program the elements of a list in a single line.(list is the parameter)
+#write a program find the factorial of n (n is the parameter)
+#write a program convert USD to INR
